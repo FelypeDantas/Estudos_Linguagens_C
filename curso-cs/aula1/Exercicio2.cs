f525f0df2ece1d9d426528e7a1ffb754c9af8b60
Felype <felyped03@gmail.com>
2026-05-29 10:50:45 -0300

    Update Exercicio2.cs

--- curso-cs/aula1/Exercicio2.cs
+++ curso-cs/aula1/Exercicio2.cs
@@ -1,16 +1,20 @@
-
 using System;
-namespace LearningCSharp{
-   class program {
-     public static void despedida (params string [] alunos) {
-     
-          foreach(var aluno in alunos){
-             Console.WriteLine("Tchau {0}, até a próxima aula", aluno);
-           }
-       }
-       
-       static void Main(){
-           despedida("João", "Antônio", "Thiago");
-       }
-   }
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        public static void Despedida(params string[] alunos)
+        {
+            foreach (string aluno in alunos)
+            {
+                Console.WriteLine($"Tchau {aluno}, até a próxima aula!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Despedida("João", "Antônio", "Thiago");
+        }
+    }
 }
