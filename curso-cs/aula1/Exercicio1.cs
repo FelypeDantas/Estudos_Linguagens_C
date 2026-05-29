78a6d76738f312768d0adeaa52d2b283fa58a22b
Felype <felyped03@gmail.com>
2026-05-29 10:49:19 -0300

    Update Exercicio1.cs

--- curso-cs/aula1/Exercicio1.cs
+++ curso-cs/aula1/Exercicio1.cs
@@ -1,5 +1,7 @@
-string curso = "C#: Criando a sua primeira aplicacao";
-string nome = "Felype";
-string sobrenome = "Dantas";
-Console.WriteLine(curso);
-Console.WriteLine("Gui Lima");
+string curso = "C#: Criando a sua primeira aplicação";
+string nomeCompleto = "Felype Dantas";
+string instrutor = "Gui Lima";
+
+Console.WriteLine($"Curso: {curso}");
+Console.WriteLine($"Aluno: {nomeCompleto}");
+Console.WriteLine($"Instrutor: {instrutor}");
